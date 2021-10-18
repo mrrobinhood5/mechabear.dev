0359a729948c9a76e866f24a51ed511771e71dd6
Rob M <mrrobinhood5@gmail.com>
2021-10-18 17:00:24 -0500

    Added cogs reload command

--- cogs/dev.py
+++ cogs/dev.py
@@ -22,6 +22,26 @@ class DevCommands(commands.Cog, name='Developer', command_attrs=dict(hidden=True
         except commands.errors.ExtensionNotFound:
             await ctx.send(f"`{cog}` does not exist!")
 
+    # @bot_checks.is_admin()
+    @commands.command(name='reload', aliases=['rl'])
+    async def reload(self, ctx, cog):
+        """
+        Reloads a cog.
+        """
+        await ctx.message.delete()
+        extensions = self.bot.extensions
+        if cog == 'all':
+            for extension in extensions:
+                self.bot.unload_extension(extension)
+                self.bot.load_extension(extension)
+            await ctx.send('Reloaded all Cogs')
+        if cog in extensions:
+            self.bot.unload_extension(cog)  # Unloads the cog
+            self.bot.load_extension(cog)  # Loads the cog
+            await ctx.send(f'Reloaded the {cog[5:].title()}, master.')  # Sends a message where content='Done'
+        else:
+            await ctx.send('Unknown Cog')  # If the cog isn't found/loaded.
+
     @commands.slash_command(description="Responds with 'World'")
     async def hello(self, inter: disnake.ApplicationCommandInteraction):
         await inter.response.send_message("World")
